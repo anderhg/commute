9ced0a59c4bfae23faf52c176d4efcd6ace47338
Øyvind Johnsen <oyvindjohnsen@yvinds-MacBook-Pro.local>
2017-02-07 15:27:04 -0800

    added profile link

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var handlebars = require('express3-handlebars')
 var index = require('./routes/index');
 var project = require('./routes/project');
 var setupSchedule = require('./routes/setupschedule');
+var profile = require('./routes/profile');
 // Example route
 // var user = require('./routes/user');
 
@@ -40,6 +41,7 @@ if ('development' == app.get('env')) {
 app.get('/', index.view);
 app.get('/project/:name', project.viewProject);
 app.get('/setupschedule', setupSchedule.viewSetupSchedule);
+app.get('/profile', profile.viewProfile);
 // Example route
 // app.get('/users', user.list);
 
